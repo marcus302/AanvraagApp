30cc900d60706f25ddead0d2528513ae16108c26
Mark de Wijk <dewijkmark@gmail.com>
2025-08-06 14:35:21 +0300

    Setup docker compose with redis.

---
+++ Dockerfile
@@ -0,0 +1,11 @@
+FROM python:3.13.3-bullseye
+
+WORKDIR /app
+
+COPY poetry.lock pyproject.toml README.md  ./
+RUN pip install poetry==2.1.3
+RUN poetry install --with dev --no-root
+COPY aanvraagapp ./aanvraagapp
+RUN poetry install --with dev
+
+CMD ["poetry", "run", "python3", "-Xfrozen_modules=off", "-m", "debugpy", "--listen", "0.0.0.0:5678", "-m", "uvicorn", "aanvraagapp:app", "--reload", "--host", "0.0.0.0", "--port", "80"]
--- aanvraagapp/database.py
+++ aanvraagapp/database.py
@@ -1,11 +1,14 @@
 from contextlib import asynccontextmanager
 from typing import AsyncGenerator
+import redis.asyncio as redis
+
 
 from sqlalchemy.ext.asyncio import AsyncSession, async_sessionmaker, create_async_engine
 
 from . import models
 
 DATABASE_URI = "sqlite+aiosqlite:///./aanvraagapp.db"
+REDIS_URI = "redis://redis:6379/0"
 
 # Create async engine and session maker
 async_engine = create_async_engine(
@@ -25,3 +28,9 @@ async def get_async_session() -> AsyncGenerator[AsyncSession, None]:
     """Async dependency that provides a database session"""
     async with async_session_maker() as session:
         yield session
+
+
+async def get_redis_client():
+    client = redis.Redis.from_pool(redis.ConnectionPool.from_url(REDIS_URI, decode_responses=True))
+    yield client
+    await client.aclose()
